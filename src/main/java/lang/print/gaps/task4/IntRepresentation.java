63a2fa17a2a2c3ea6963d6b698a59a342f4dde08
Sardor0305 <87496899+Sardor0305@users.noreply.github.com>
2023-06-07 14:29:51 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+   
+        int intC = (int) c;
+        int intH = (int) h;
+        int intA = (int) a;
+        int intR = (int) r;
+
+        System.out.println("Integer representation of 'c': " + intC);
+        System.out.println("Integer representation of 'h': " + intH);
+        System.out.println("Integer representation of 'a': " + intA);
+        System.out.println("Integer representation of 'r': " + intR);
+        
     }
 }
